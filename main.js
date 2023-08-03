6065141e83c940fa652191538990dd914b3fcc49
Willian Gabriel <willian.minikovski@hotmail.com>
2023-08-03 14:13:03 -0300

    sim

---
+++ main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules:{
+            nome: {
+                required: true
+            }, 
+            email: {
+                required: true,
+                email: true,
+
+            },
+            telefone: {
+                required: true
+            }
+          }, 
+            messages: {
+                nome: 'Por favor, insira seu nome'
+          }, 
+          submitHandler: function(form) {
+                console.log(form)
+                $('#nome').val('');
+                $('#email').val('');
+                $('#telefone').val('');
+                $('#mensagem').val('');
+            },
+            invalidHandler: function(evento, validador) {
+                let camposIncorretos = validador.numberOfInvalids();
+                if(camposIncorretos) {
+                    alert(`Existem ${camposIncorretos} campos incorretos`)
+                        }
+                    }
+                })
+
+
+
+
+
+
+
+
+
+
+
+});
